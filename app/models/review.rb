fdd6a041822ae6b0a3a5b246af526861394791b4
Fanny Farrouch <fanny.farrouch@gmail.com>
2022-06-18 15:32:04 +0200

    Review validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :adoption
   belongs_to :user
+
+  validates :rating, presence: true, length: { in: 0..5 }
+  validates :description, presence: true, length: { in: 3..114}
 end
