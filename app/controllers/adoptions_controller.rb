de9d8691495bb521bc8a872d5b0eaabddcaf02f8
Fanny Farrouch <fanny.farrouch@gmail.com>
2022-06-20 16:28:03 +0200

    Initiating new adoption feature

--- app/controllers/adoptions_controller.rb
+++ app/controllers/adoptions_controller.rb
@@ -1,2 +1,20 @@
 class AdoptionsController < ApplicationController
+  def new
+    @tree = Tree.find(params[:tree_id])
+    @adoption = Adoption.new
+  end
+
+  def create
+    @adoption = Adoption.new(adoption_params)
+    @tree = Tree.find(params[:tree_id])
+    @adoption.tree = @tree
+    @adoption.save
+    redirect_to tree_path(@tree)
+  end
+
+  private
+
+  def adoption_params
+    params.require(:adoption).permit(:starts_at, :ends_at, :user, :tree)
+  end
 end
